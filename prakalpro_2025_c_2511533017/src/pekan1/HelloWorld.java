c4cdf9827bfa72ab1c3eed1d68da95707413c163
FTI-LAB03-PC16 <FTI-LAB03-PC16@FTI-LAB03-PC16>
2025-09-09 11:58:35 +0700

    pratikum 1 kelas c

---
+++ prakalpro_2025_c_2511533017/src/pekan1/HelloWorld.java
@@ -0,0 +1,20 @@
+package pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//program pertama
+		/*komentar
+		 * dua baris atau lebih
+		 * pertemuan 1 pratikum alpro
+		 */
+		System.out.println("HelloWorld");
+		System.out.println();
+		System.out.println("program ini nmenghasilkan");
+		System.out.println("empat baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+		System.out.println("this"+" program prints a\n"+"quote from the Gettysbrug Address.");
+		
+	}
+
+}
