4047121bf66bf552f7c969e73927bed896ec402b
Digo Yuandra <Digo Yuandra@GOYU>
2025-10-29 16:43:49 +0700

    Pratikum Pekan 5 nim:2511533017

---
+++ prakalpro_2025_c_2511533017/prakalpro_2025_c_2511533017/src/pekan5/PeluangFor2_25115330117.java
@@ -0,0 +1,12 @@
+package pekan5;
+
+public class PeluangFor2_25115330117 {
+
+		public static void main(String[] args) {
+			for (int i=1; i <= 10; i++) {
+				System.out.print(i+" "); 
+			}
+		}
+
+	}
+
